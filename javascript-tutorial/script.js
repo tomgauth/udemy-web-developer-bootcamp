f8298da13fa1421d360725f05d3751d6b0c24c75
tom gauthier <tomgauthier0@gmail.com>
2017-03-20 23:12:11 +0100

    experimenting with JS

---
+++ javascript-tutorial/script.js
@@ -0,0 +1,40 @@
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var fullName = firstName + " " + lastName;
+// var age = prompt("What is your age?");
+
+// console.log("youf full name is " +  fullName + " and you are " + age + " years old!")
+
+
+// var age = prompt("What is your age?");
+// var ageInDays = age * 365;
+// alert("your are roughtly " + ageInDays + " days old")
+
+
+// create secret number
+
+// var secretNumber = Math.floor((Math.random() * 20) + 1)
+
+var secretNumber = 4;
+// ask user for a guess
+
+var userNumber = Number(prompt("Guess the right number"));
+
+// say if it's higher or lower
+
+while (secretNumber !== userNumber){
+  guessNumber;
+}
+
+function guessNumber() {
+   if (secretNumber < userNumber) {
+    var userNumber = prompt("the secret number is lower");
+  } else if (secretNumber > userNumber) {
+    var userNumber = prompt("the secret number is higher!");
+  }
+}
+
+
+alert("well done!");
+
+// say "you won" if the number is right
