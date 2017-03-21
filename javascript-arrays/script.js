33b00ca71563e66cf9faf1ee8309d93a975b4951
tom gauthier <tomgauthier0@gmail.com>
2017-03-21 17:20:26 +0100

    minor changes

--- javascript-arrays/script.js
+++ javascript-arrays/script.js
@@ -1,23 +1,45 @@
 var todolist = [];
 
-var answer = prompt("what would you like to do: add - list - quit");
+var answer = prompt("what would you like to do: new - list - quit");
 
 while (answer != "quit") {
 
-  if (answer == "add") {
-    newtask =  prompt("give a name to your task");
-    todolist.push(newtask);
-    alert(newtask + " has been added to the todolist");
-    answer;
+  if (answer == "new") {
+    newTodo();
   }
 
   else if (answer == "list") {
-    alert(todolist);
+    listTodo();
+  }
+
+  else if (answer == "delete") {
+    deleteTodo();
   }
 
   else {
     alert("I don't understand");
   }
-var answer = prompt("what would you like to do: add - list - quit");
+  var answer = prompt("what would you like to do: new - list - quit");
+}
+alert("Goodbye!")
+
+
+function newTodo() {
+  newtask =  prompt("give a name to your task");
+  todolist.push(newtask);
+  alert(newtask + " has been newed to the todolist");
+}
+
+function listTodo() {
+  todolist.forEach(function(todo, i){
+    console.log(i + ": " + todo);
+    alert(todo);
+  });
+}
+
+function deleteTodo() {
+  element = prompt("what element do you want to delete?")
+  todos.splice(index,1); // index position and number of items to delete
+  alert("deleted!");
 }
 
