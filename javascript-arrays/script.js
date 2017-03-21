8a9c21085181b1a7545d88d6a8c71f529c249229
tom gauthier <tomgauthier0@gmail.com>
2017-03-21 16:02:48 +0100

    my todo app works fine

--- javascript-arrays/script.js
+++ javascript-arrays/script.js
@@ -1,29 +1,23 @@
-// Todo list is an empty array
-
-// Start with a prompt "what would you like to do: add - list - quit"
-
-// Add todo
 var todolist = [];
 
 var answer = prompt("what would you like to do: add - list - quit");
 
-while (!= quit) {
+while (answer != "quit") {
+
   if (answer == "add") {
     newtask =  prompt("give a name to your task");
     todolist.push(newtask);
     alert(newtask + " has been added to the todolist");
+    answer;
   }
 
-  // List todo
   else if (answer == "list") {
-    // just display the elements of the array
+    alert(todolist);
   }
 
   else {
-    prompt "I don't understand, what would you like to do: add - list - quit"
+    alert("I don't understand");
   }
+var answer = prompt("what would you like to do: add - list - quit");
 }
 
-// Quit todo
-  // when answer == quit, quit the while loop
-  // while answer != quit, run the loop
