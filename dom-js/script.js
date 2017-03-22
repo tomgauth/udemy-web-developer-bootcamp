662056aee05c9eee8afe50ba0e56c4ffded30a25
tom gauthier <tomgauthier0@gmail.com>
2017-03-22 21:06:13 +0100

    started exercise with DOM manipulation, most code written in console

---
+++ dom-js/script.js
@@ -0,0 +1,15 @@
+var body = document.querySelector("body");
+
+var isBlue = false;
+
+// setInterval(function(){
+//   if (isBlue){
+//     body.style.background = "white";
+//     console.log(isBlue);
+//   } else {
+//     body.style.background = "blue";
+//     console.log(isBlue);
+//   }
+//   isBlue = !isBlue;
+// }, 1000);
+
