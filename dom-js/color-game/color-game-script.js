5780f3596041d4af2583abd656325aeb26c41c44
tom gauthier <tomgauthier0@gmail.com>
2017-03-24 17:55:59 +0100

    added first feature

--- dom-js/color-game/color-game-script.js
+++ dom-js/color-game/color-game-script.js
@@ -1,73 +1,33 @@
-// select the buttons
-var buttonLeft = document.getElementById("button-left");
-var buttonRight = document.getElementById("button-right");
-var buttonReset = document.getElementById("button-reset");
+var colors = [
+  "rgb(255,0,0)",
+  "rgb(255,255,0)",
+  "rgb(255,0,255)",
+  "rgb(255,255,255)",
+  "rgb(0,0,0)",
+  "rgb(0,0,255)"
+]
 
-// select the elements that will change during the game
-var scoreLeft = document.querySelector("#score-left");
-var scoreRight = document.querySelector("#score-right");
-var numInput = document.querySelector("#num-input");
-var winningScore = document.querySelector("#winning-score");
 
-// default values when the page loads
-var numWinningScore = 5;
-var numScoreLeft = 0;
-var numScoreRight = 0;
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("color-displayed");
 
-// the game works unless it's gameover
-var gameOver = false;
+colorDisplay.textContent = pickedColor
 
-// increment the left score by one unless it's equal to the winning score
-buttonLeft.addEventListener("click", function(){
-  if (!gameOver) {
-    numScoreLeft ++;
-    if (numScoreLeft === numWinningScore){
-      scoreLeft.classList.add("winner");
-      gameOver = true;
-    }
-    scoreLeft.textContent = numScoreLeft;
+for (var i = 0 ; i <= squares.length ; i++) {
+  // add initial colors to squares
+  squares[i].style.background = colors[i];
+  // !Error, style Uncaught TypeError: Cannot read property 'style' of undefined
+
+  // add click listeners to squares
+  squares[i].addEventListener("click", function(){
+    // grab color of picked square
+    var clickedColor = this.style.background;
+    //compare color to picked color
+    if (pickedColor === clickedColor) {
+      alert("Correct!");
+    }else{
+      alert("not correct!");
     }
   });
-
-// increment the right score by one unless it's equal to the winning score
-buttonRight.addEventListener("click", function(){
-  if (!gameOver){
-    numScoreRight ++;
-    if (numScoreRight === numWinningScore){
-      gameOver = true;
-      scoreRight.classList.add("winner");
-    }
-    scoreRight.textContent = numScoreRight;
-    }
-});
-
-// resets the score
-function reset(){
-  gameOver = false;
-
-  numScoreLeft = 0;
-  scoreLeft.textContent = numScoreLeft;
-  scoreLeft.classList.remove("winner");
-
-  numScoreRight = 0;
-  scoreRight.textContent = numScoreRight;
-  scoreRight.classList.remove("winner");
 }
-
-// resets the score when button reset is pressed
-buttonReset.addEventListener("click", function(){
-  reset();
-});
-
-// use the input form to change the value of the winning score
-numInput.addEventListener("change", function(){
-
-  reset(); // resets the score to avoid having current score higher than new winning score
-  console.log(winningScore);
-  winningScore.textContent = this.value;
-  numWinningScore = Number(this.value);
-  return numWinningScore;
-});
-
-
-// end of number game code
