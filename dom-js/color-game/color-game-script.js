0640aeb446a8cc0968d05befee21f454d2f27eb6
tom gauthier <tomgauthier0@gmail.com>
2017-03-25 10:56:28 +0100

    debugged

--- dom-js/color-game/color-game-script.js
+++ dom-js/color-game/color-game-script.js
@@ -1,10 +1,10 @@
 var colors = [
-  "rgb(255,0,0)",
-  "rgb(255,255,0)",
-  "rgb(255,0,255)",
-  "rgb(0,255,255)",
-  "rgb(0,0,0)",
-  "rgb(0,0,255)"
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 0)",
+  "rgb(0, 0, 255)"
 ]
 
 
@@ -14,11 +14,11 @@ var colorDisplay = document.getElementById("color-displayed");
 
 colorDisplay.textContent = pickedColor
 
-for (var i = 0 ; i <= squares.length ; i++) {
+for (var i = 0 ; i < squares.length ; i++) {
   // add initial colors to squares
-  squares[i].style.background = colors[i];
+  squares[i].style.background = colors[i]
+  console.log(squares[i].style.background);
   // !Error, style Uncaught TypeError: Cannot read property 'style' of undefined
-
   // add click listeners to squares
   squares[i].addEventListener("click", function(){
     // grab color of picked square
