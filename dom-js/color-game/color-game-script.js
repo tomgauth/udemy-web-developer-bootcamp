60983e33997358ece8fdd14c0132dd82684fe8e6
tom gauthier <tomgauthier0@gmail.com>
2017-03-27 11:53:40 +0200

    color game done with nice refacto

--- dom-js/color-game/color-game-script.js
+++ dom-js/color-game/color-game-script.js
@@ -1,39 +1,90 @@
-var colors = generateRandomColors(6);
-
-
+var numOfSquares = 6;
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById("color-displayed");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+var modeButtons = document.querySelectorAll(".mode");
 
-colorDisplay.textContent = pickedColor
+init();
 
-for (var i = 0 ; i < squares.length ; i++) {
-  // add initial colors to squares
-  squares[i].style.background = colors[i]
-  console.log(squares[i].style.background);
-  // !Error, style Uncaught TypeError: Cannot read property 'style' of undefined
-  // add click listeners to squares
-  squares[i].addEventListener("click", function(){
-    // grab color of picked square
-    var clickedColor = this.style.background;
-    //compare color to picked color
-    if (pickedColor === clickedColor) {
-      messageDisplay.textContent = "You Won!";
-      changeColors(clickedColor);
-      h1.style.background = clickedColor;
-    }else{
-      this.style.background = "#1c1c1f";
-      messageDisplay.textContent = "try again!";
-    }
-  });
+function init(){
+  setUpModeButtons();
+  setUpSquareListeners();
+  reset();
 }
+
+
+function setUpModeButtons(){
+  for (var i = 0 ; i < modeButtons.length ; i++) {
+    modeButtons[i].addEventListener("click", function(){
+      modeButtons[0].classList.remove("selected");
+      modeButtons[1].classList.remove("selected");
+      this.classList.add("selected");
+      // figure how how many squares to show
+      this.textContent === "Easy" ? numOfSquares = 3 : numOfSquares = 6;
+      reset();
+    });
+  }
+}
+
+function setUpSquareListeners(){
+  for (var i = 0 ; i < squares.length ; i++) {
+    // add initial colors to squares
+    // add click listeners to squares
+    squares[i].addEventListener("click", function(){
+      // grab color of picked square
+      var clickedColor = this.style.background;
+      //compare color to picked color
+      if (pickedColor === clickedColor) {
+        messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again?";
+        changeColors(clickedColor);
+        h1.style.background = clickedColor;
+      }else{
+        this.style.background = "#1c1c1f";
+        messageDisplay.textContent = "try again!";
+      }
+    });
+  }
+}
+
+function reset(){
+  //generate all new colors
+  colors = generateRandomColors(numOfSquares);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change color display to match the new colors
+  colorDisplay.textContent = pickedColor
+  // reset textContent of reset button
+  resetButton.textContent = "New Colors"
+  //reset message displayed
+  messageDisplay.textContent = "";
+  for (var i = 0 ; i < squares.length ; i++) {
+    if (colors[i]){
+      squares[i].style.display = "block";
+      squares[i].style.background = colors[i];
+    }else{
+      squares[i].style.display = "none";
+    }
+  }
+  h1.style.background = "steelblue"
+}
+
+
+//reset Button
+resetButton.addEventListener("click", function(){
+  reset();
+});
+
 
 function changeColors(color){
   for (var i = 0 ; i < squares.length ; i++) {
-    squares[i].style.background = pickedColor;
+    squares[i].style.background = color;
   };
+  console.log(squares.length)
 }
 
 function pickColor(){
@@ -48,9 +99,9 @@ function generateRandomColors(num){
   for (var i = 0; i < num; i++){
     // get random color and push into array
     arr.push(randomColor());
-  };
-  return arr
-};
+  }
+  return arr;
+}
 
 
 function randomColor(){
@@ -62,5 +113,6 @@ function randomColor(){
   var blue = Math.floor(Math.random() * 256);
   return "rgb("+red+", "+green+", "+blue+")"
 }
+
 
 
